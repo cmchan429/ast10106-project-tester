91d3193d6faf52046d4110d31a9487f94092e31c
Lai Hin Ng <laihinng2-c@my.cityu.edu.hk>
2018-11-03 13:34:24 +0800

    Task Submit (Dan)

---
+++ src/Dan_11636.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Dan_11636 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n, i, count;
+		count = 1;
+		
+		while (true) {
+			n = sc.nextInt();
+			if (n == -1) break;
+			
+			for (i = 0; Math.pow(2, i) < n; i++) {}
+			
+			System.out.printf("case %d: %d\n", count, i);
+			count++;
+		}
+		
+		sc.close();
+	}
+}
